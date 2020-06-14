1ce562020ed535efbc110609283e8556dc31e09f
Paras Buda <parasbuda1234@gmail.com>
2020-06-15 00:46:12 +0545

    heading or navbar is made

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import {Switch,Route} from "react-router-dom"
 import HomePage from "./pages/homepage/HomePage"
 import Shop from "./pages/ShopPage/Shop"
+import Header from './components/header/Header';
 function App() {
   return (
     <div >
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/shop" component={Shop}></Route>
