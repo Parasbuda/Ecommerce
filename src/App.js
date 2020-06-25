b948e932bbd42b1a8beae48979e7f21a51f704f7
Paras Buda <parasbuda1234@gmail.com>
2020-06-26 02:41:17 +0545

    Redirecting the user and better redux action and types

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Switch,Route} from "react-router-dom"
+import {Switch,Route,Redirect} from "react-router-dom"
 import HomePage from "./pages/homepage/HomePage"
 import Shop from "./pages/ShopPage/Shop"
 import Header from './components/header/Header';
@@ -52,14 +52,21 @@ class App extends Component{
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/shop" component={Shop}></Route>
-        <Route path="/signin" component={SignInSignUp}></Route>
+        <Route path="/signin" render={()=>this.props.currentUser?
+        (<Redirect to="/"/>):(
+          <SignInSignUp/>
+        )}></Route>
       </Switch>
     </div>
   );
 }
 }
+
+const mapStateToProps=({user})=>({
+  currentUser:user.currentUser 
+})
 const mapDispatchToProps=dispatch=>({
   setCurrentUser:user=>dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
